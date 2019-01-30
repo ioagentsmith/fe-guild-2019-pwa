71f3641db3acc530475043fbab910aecbf5a1fa0
Christoff Smith <Christoff.Smith@ing.com>
2019-01-30 15:16:08 +0100

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ const manifest = {
             'sizes': '512x512'
         }
     ],
-    //"start_url": "index.html",
+    //'start_url': '/index.html',
     //"scope": ".",
     'display': 'standalone',
     'orientation': 'portrait-primary',
@@ -48,7 +48,7 @@ const manifest = {
     'dir': 'ltr',
     'lang': 'en-US'
 };
-let deferredPrompt;
+
 
 window.addEventListener('load', () => {
     const base = document.querySelector('base');
@@ -69,27 +69,26 @@ window.addEventListener('load', () => {
     const manifestURL = URL.createObjectURL(blob);
     document.querySelector('#manifestPlaceholder').setAttribute('href', manifestURL);
 
+    // After the existing code
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register(`${baseUrl}sw.js`)
-            .then(registration => {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            })
-            .catch(err => {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            });
+            .then( registration => {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(err => {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     }
 });
 
-window.addEventListener('beforeinstallprompt', event => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    event.preventDefault();
+self.addEventListener('install', event => {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(self.skipWaiting());
+});
 
-    console.log('beforeinstallprompt fired');
-
-    // Stash the event so it can be triggered later.
-    deferredPrompt = event;
-
-    return false;
+self.addEventListener('activate', event => {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
 });
